3b61fdb577394873ef20032135badf17590c41e4
lavitz <leojimlavi@gmail.com>
2022-05-10 20:44:57 -0500

    React mapbox and Marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import RoomRoundedIcon from "@mui/icons-material/RoomRounded";
+
 function App() {
-  return <div className="App"></div>;
+  const [viewState, setViewState] = useState({
+    longitude: 17,
+    latitude: 46,
+    zoom: 4,
+  });
+  return (
+    <Map
+      initialViewState={viewState}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/jaruno/cl30wxtir007315p5ut8yvqpp"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    >
+      <Marker
+        longitude={2.294694}
+        latitude={48.858093}
+        offsetLeft={-20}
+        offsetTop={-10}
+      >
+        <RoomRoundedIcon
+          sx={{
+            color: "tomato",
+            fontSize: viewState.zoom * 7,
+            "&:hover": {
+              transform: "scale(1.1)",
+              cursor: "pointer",
+              filter: "brightness(1.5)",
+            },
+          }}
+        />
+      </Marker>
+    </Map>
+  );
 }
 
 export default App;

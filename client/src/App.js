fb88068d70cb9b518406803c24a12b561b14eb5c
lavitz <leojimlavi@gmail.com>
2022-05-13 00:18:41 -0500

    Camera Transition & Adding new location design

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
 import Map, { Marker, Popup } from "react-map-gl";
@@ -54,6 +54,73 @@ const Author = styled.div`
   margin-bottom: 5px;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: 100%;
+  height: 100%;
+
+  label {
+    font-size: 14px;
+    margin-bottom: 5px;
+    color: crimson;
+    border-bottom: 3px solid;
+  }
+
+  input {
+    font-size: 14px;
+    border: none;
+    outline: none;
+    margin-bottom: 5px;
+    border-bottom: 3px solid;
+    border-color: black;
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    align-content: center;
+  }
+
+  textarea {
+    font-size: 14px;
+    outline: none;
+    padding: 5px;
+  }
+
+  Select {
+    font-size: 14px;
+    border: none;
+    outline: none;
+    margin-bottom: 5px;
+  }
+
+  button {
+    font-size: 14px;
+    margin-bottom: 5px;
+    padding: 5px;
+    background-color: transparent;
+    border: 0.5px solid black;
+    transition: all 0.2s ease-in-out;
+    border-radius: 5px;
+    font-weight: bold;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      background-color: black;
+      color: crimson;
+      cursor: pointer;
+      transform: scale(1);
+      transition: all 0.2s ease-in-out;
+    }
+  }
+`;
+
 function App() {
   const currentUser = "Marcus";
   const [pins, setPins] = useState([]);
@@ -63,6 +130,8 @@ function App() {
     zoom: 4,
   });
   const [selectedPin, setSelectedPin] = useState(null);
+  const [location, setLocation] = useState(null);
+  const mapRef = useRef();
 
   useEffect(() => {
     const getPins = async () => {
@@ -76,17 +145,28 @@ function App() {
     getPins();
   }, []);
 
-  const handleMarkerClick = (id) => {
+  const handleMarkerClick = (id, lat, lon) => {
     setSelectedPin(id);
+    mapRef.current?.flyTo({ center: [lon, lat], duration: 2002 });
+  };
+
+  const handleAddClick = (e) => {
+    e.originalEvent.stopPropagation();
+    const { lng, lat } = e.lngLat;
+    setLocation({ lat: lat, lon: lng });
   };
 
   return (
     <Map
+      ref={mapRef}
       {...viewState}
       onMove={(evt) => setViewState(evt.viewState)}
       style={{ width: "100vw", height: "100vh" }}
-      mapStyle="mapbox://styles/jaruno/cl30wxtir007315p5ut8yvqpp"
+      mapStyle="mapbox://styles/mapbox/light-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      onDblClick={(e) => {
+        handleAddClick(e);
+      }}
     >
       {pins.map((pin) => (
         <div key={pin._id}>
@@ -100,7 +180,7 @@ function App() {
               // If we let the click event propagates to the map, it will immediately close the popup
               // with `closeOnClick: true`
               e.originalEvent.stopPropagation();
-              handleMarkerClick(pin._id);
+              handleMarkerClick(pin._id, pin.lat, pin.lon);
             }}
           >
             <RoomRoundedIcon
@@ -121,6 +201,7 @@ function App() {
               latitude={pin.lat}
               anchor="bottom-right"
               focusAfterOpen={false}
+              closeOnClick={true}
               onClose={() => setSelectedPin(null)}
               offsetTop={-10}
               offsetLeft={-20}
@@ -143,6 +224,38 @@ function App() {
                 </Author>
                 <span>{format(pin.createdAt)}</span>
               </Card>
+            </Popup>
+          )}
+          {location?.lon && (
+            <Popup
+              longitude={location.lon}
+              latitude={location.lat}
+              anchor="bottom-right"
+              focusAfterOpen={false}
+              closeOnClick={true}
+              onClose={() => setLocation(null)}
+              offsetTop={-10}
+              offsetLeft={-20}
+            >
+              <Form>
+                <label>Title</label>
+                <input placeholder="Enter a title" />
+                <label>Review</label>
+                <textarea
+                  placeholder="Say us something about this place."
+                  rows="5"
+                  cols="25"
+                />
+                <label>Rating</label>
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button type="submit">Add Pin</button>
+              </Form>
             </Popup>
           )}
         </div>
